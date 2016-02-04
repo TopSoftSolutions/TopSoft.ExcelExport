faeb00b5191ef7073abe1bcc3fea6c2bf5a56cd3
AramKocharyan <aram.koch@gmail.com>
2016-02-04 16:50:58 +0400

    added missing files

---
+++ TopSoft.ExcelExport/Entity/ExcelCell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace TopSoft.ExcelExport.Entity
+{
+    public class ExcelCell
+    {
+        public List<OpenXmlElement> Styles { get; set; }
+        public Cell Cell { get; set; }
+
+        public ExcelCell()
+        {
+            Styles = new List<OpenXmlElement>();
+            Cell = new Cell();
+        }
+    }
+}
---
+++ TopSoft.ExcelExport/Helpers/AttrStylesHelper.cs
@@ -0,0 +1,80 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Spreadsheet;
+using TopSoft.ExcelExport.Attributes;
+
+namespace TopSoft.ExcelExport.Helpers
+{
+    static class AttrStylesHelper
+    {
+        static public Font GetFont(this CellTextAttribute cellTextAttribute)
+        {
+            Font retFont = new Font();
+
+            if(cellTextAttribute.Bold)
+            {
+                retFont.Append(new Bold());
+            }
+
+            if(cellTextAttribute.Italic)
+            {
+                retFont.Append(new Italic());
+            }
+
+            if(cellTextAttribute.Underliine)
+            {
+                retFont.Append(new Underline());
+            }
+            return retFont;
+        }
+
+        static public PatternFill GetFill(this CellFillAttribute cellFillAttribute)
+        {
+             PatternFill retFill = new PatternFill() { PatternType = PatternValues.Solid };
+
+            if(!string.IsNullOrEmpty(cellFillAttribute.HexColor))
+            {
+                
+                retFill.ForegroundColor = new ForegroundColor() { Rgb = HexBinaryValue.FromString(cellFillAttribute.HexColor) };
+            }
+
+            return retFill;
+        }
+
+        static public Border GetBorder(this CellBorderAttribute cellFontAttribute)
+        {
+            Border retBorder = new Border();
+
+            if(cellFontAttribute.LeftBorder)
+            {
+                retBorder.LeftBorder = new LeftBorder() { Style = BorderStyleValues.Thin };
+                retBorder.LeftBorder.Color = new Color() { Indexed = 64U };
+            }
+
+            if(cellFontAttribute.RightBorder)
+            {
+                retBorder.RightBorder = new RightBorder() { Style = BorderStyleValues.Thin };
+                retBorder.RightBorder.Color = new Color() { Indexed = 64U };
+            }
+
+            if(cellFontAttribute.TopBorder)
+            {
+                retBorder.TopBorder = new TopBorder() { Style = BorderStyleValues.Thin };
+                retBorder.TopBorder.Color = new Color() { Indexed = 64U };
+            }
+
+            if(cellFontAttribute.BottomBorder)
+            {
+                retBorder.BottomBorder = new BottomBorder() { Style = BorderStyleValues.Thin };
+                retBorder.BottomBorder.Color = new Color() { Indexed = 64U };
+            }
+
+            if(cellFontAttribute.DiagonalBorder)
+            {
+                retBorder.DiagonalBorder = new DiagonalBorder() { Style = BorderStyleValues.Thin };
+                retBorder.DiagonalBorder.Color = new Color() { Indexed = 64U };
+            }
+
+            return retBorder;
+        }
+    }
+}
